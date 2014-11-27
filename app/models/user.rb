c5681a306b32f6e2f9d813d2d156a1a7860784e7
carper33 <carla.lahoz.perez@gmail.com>
2014-11-27 10:30:20 +0000

    un poco de style

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ActiveRecord::Base
 	has_many :posts
 
+	validates :nombre, presence:true
+	validates :email, presence:true
+	validates :ciudad, presence:true
+
+	validate :project_exists
 	def file_column
 	file_column :image
 	end
