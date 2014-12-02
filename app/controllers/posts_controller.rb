1b146d0b88240bc83adc4900db0493c3d00caf84
Enric Lluelles <enric@lluell.es>
2014-12-02 21:45:34 +0100

    Filling up posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
-  before_action :find_user
+  before_action :find_user, except: :all
+  before_action :find_post, except: [:all, :index, :new]
+
+  helper_method :can_edit?, :can_create?
 
   # GET /posts
   # GET /posts.json
@@ -7,16 +10,58 @@ class PostsController < ApplicationController
     @posts = @user.posts
   end
 
+  def new
+    @post = Post.new(user_id: current_user.id)
+  end
+
+  def create
+    @post = @user.posts.create(post_params)
+    redirect_to post_path(@user.username, @post)
+  end
+
 
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @post = @user.posts.find_by_slug(params[:id])
   end
 
   def find_user
     @user = User.find_by_username(params[:user_name])
   end
-             
+
+  def find_post
+    @post = @user.posts.find_by_slug(params[:id])
+  end
+
+  def all
+    @posts = Post.all
+  end
+
+  def edit
+    if current_user != @post.user
+      render text: "Not found", status: 404
+    else
+      render action: "edit"
+    end
+  end
+
+  def update
+    @post.update_attributes(post_params)
+    redirect_to post_path(@post.user.username, @post)
+  end
+
+  def can_edit?(post)
+    post.user == current_user
+  end
+
+  def can_create?
+    @user == current_user
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 
 end
