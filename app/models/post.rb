c0a0c562b8cce2296907e3dd7396501ebb39c2a1
Enric Lluelles <enric@lluell.es>
2014-12-02 21:43:41 +0100

    to_param

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,13 @@
 class Post < ActiveRecord::Base
-	  belongs_to :user
+  belongs_to :user
+  before_save :set_slug
 
-	  before_save :set_slug
 
+  def set_slug
+    self.slug = self.title.parameterize
+  end
 
-	  def set_slug
-	  	self.slug = self.title.parameterize
-	  end
+  def to_param
+    self.slug
+  end
 end
