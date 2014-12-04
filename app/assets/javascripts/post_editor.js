163a3dc61ab1433d7830a9161ce7edc61750ea6e
Enric Lluelles <enric@lluell.es>
2014-12-04 21:35:37 +0100

    Adding views for picture uploading and some styles

--- app/assets/javascripts/post_editor.js
+++ app/assets/javascripts/post_editor.js
@@ -1,4 +1,6 @@
 //= require marked.min
+//= require jquery.uploadfile
+//
 window.throttle = function(cb, time) {
   var timer;
 
@@ -11,18 +13,40 @@ window.throttle = function(cb, time) {
 $(document).ready(function() {
   var postBody = $("#post_body");
   var renderedPostBody = $("#rendered_post_body");
-  postBody.keydown(throttle(function() {
+
+  var refresh = throttle(function() {
     var processedBody = marked(postBody.val());
     renderedPostBody.html(processedBody);
     postBody.height(renderedPostBody.height());
-  }, 200));
+  }, 200)
 
-
-
+  postBody.keydown(refresh);
 
 
   var imageEditor = $("#image_editor_footer");
   imageEditor.click(function() {
     imageEditor.toggleClass("active");
   });
+
+  $("#picture-upload-button").uploadFile({
+    url:"/pictures",
+    fileName:"picture",
+    dragDrop: false,
+    onSuccess:function(files,data,xhr) {
+      $("#all-pictures").append(data);
+    },
+    showStatusAfterSuccess:false
+  });
+
+  $(document).on("click", "#all-pictures img", function() {
+    var imageUrl = this.src;
+    var valueToInsert = "![](" + imageUrl + ")";
+    var currentPosition = postBody.prop('selectionStart');
+    var currentValue = postBody.val();
+    var newValue = currentValue.substring(0, currentPosition) + valueToInsert + currentValue.substring(currentPosition)
+    postBody.val(newValue);
+    refresh();
+  });
+
+  refresh();
 });
